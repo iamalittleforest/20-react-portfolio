021950ea01cfd144b8a0ab3810d838e3789722e4
Wendy Kobayashi <iamalittleforest@gmail.com>
2021-07-30 11:46:34 -0700

    add icon links for GitHub, LinkedIn, and Twitter

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,8 +3,10 @@ import '../styles/Footer.css';
 
 export default function Footer() {
   return (
-    <div>
-
-    </div>
+    <footer className="page-footer footer">
+      <a className="btn-floating btn-link" href="https://www.github.com/iamalittleforest" target="_blank" rel="noreferrer"><i className="fab fa-github"></i></a>
+      <a className="btn-floating btn-link"  href="https://www.linkedin.com/in/wendy-kobayashi" target="_blank" rel="noreferrer"><i className="fab fa-linkedin-in"></i></a>
+      <a className="btn-floating btn-link"  href="https://twitter.com/iamalilforest" target="_blank" rel="noreferrer"><i className="fab fa-twitter"></i></a>
+    </footer>
   );
 }
