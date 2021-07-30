86fe823e6d7adb224d9cd171d4759d001b9156ba
Wendy Kobayashi <iamalittleforest@gmail.com>
2021-07-30 16:31:48 -0700

    add fontawesome free-solid-svg-icons

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars } from '@fortawesome/free-solid-svg-icons'
 import '../styles/Navigation.css';
 
 export default function Navigation() {
@@ -6,7 +8,9 @@ export default function Navigation() {
     <div>
       <nav className="navigation">
         <div class="container nav-wrapper">  
-          <a href="#" data-target="mobile-demo" className="sidenav-trigger"><i class="fas fa-bars"></i></a>
+          <a href="#" data-target="mobile-demo" className="sidenav-trigger">
+            <FontAwesomeIcon icon={faBars} />
+          </a>
           <ul id="nav-mobile" className="right hide-on-med-and-down">
             <li><a className="nav-link" href="#about-container" alt="About">ABOUT</a></li>
             <li><a className="nav-link" href="#portfolio-container" alt="Portfolio">PORTFOLIO</a></li>
