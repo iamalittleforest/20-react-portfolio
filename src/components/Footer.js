889b8dba0b393fd88fad3063a6ee26b589fe994f
Wendy Kobayashi <iamalittleforest@gmail.com>
2021-07-30 13:12:04 -0700

    add fontawesome icons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,20 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGithub, faLinkedin, faTwitter } from '@fortawesome/free-solid-svg-icons'
 import '../styles/Footer.css';
 
 export default function Footer() {
   return (
     <footer className="page-footer footer">
-      <a className="btn-floating btn-link" href="https://www.github.com/iamalittleforest" target="_blank" rel="noreferrer"><i className="fab fa-github"></i></a>
-      <a className="btn-floating btn-link"  href="https://www.linkedin.com/in/wendy-kobayashi" target="_blank" rel="noreferrer"><i className="fab fa-linkedin-in"></i></a>
-      <a className="btn-floating btn-link"  href="https://twitter.com/iamalilforest" target="_blank" rel="noreferrer"><i className="fab fa-twitter"></i></a>
+      <a className="btn-floating btn-link" href="https://www.github.com/iamalittleforest" target="_blank" rel="noreferrer">
+        <FontAwesomeIcon icon={faGithub} />
+      </a>
+      <a className="btn-floating btn-link"  href="https://www.linkedin.com/in/wendy-kobayashi" target="_blank" rel="noreferrer">
+        <FontAwesomeIcon icon={faLinkedin} />
+      </a>
+      <a className="btn-floating btn-link"  href="https://twitter.com/iamalilforest" target="_blank" rel="noreferrer">
+        <FontAwesomeIcon icon={faTwitter} />
+      </a>
     </footer>
   );
 }
