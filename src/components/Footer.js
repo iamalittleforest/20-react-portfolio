c7fe38d0c8fc9b75e14a4de19a15e2dbaaeaac5b
Wendy Kobayashi <iamalittleforest@gmail.com>
2021-07-30 13:37:39 -0700

    format to add styling

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,15 +6,19 @@ import '../styles/Footer.css';
 export default function Footer() {
   return (
     <footer className="page-footer footer">
-      <a className="btn-floating btn-link" href="https://www.github.com/iamalittleforest" target="_blank" rel="noreferrer">
-        <FontAwesomeIcon icon={faGithub} />
-      </a>
-      <a className="btn-floating btn-link"  href="https://www.linkedin.com/in/wendy-kobayashi" target="_blank" rel="noreferrer">
-        <FontAwesomeIcon icon={faLinkedin} />
-      </a>
-      <a className="btn-floating btn-link"  href="https://twitter.com/iamalilforest" target="_blank" rel="noreferrer">
-        <FontAwesomeIcon icon={faTwitter} />
-      </a>
+      <div className="container">
+        <div className="row center row-padding">
+          <a className="btn-floating btn-link" href="https://www.github.com/iamalittleforest" target="_blank" rel="noreferrer">
+            <FontAwesomeIcon icon={faGithub} />
+          </a>
+          <a className="btn-floating btn-link"  href="https://www.linkedin.com/in/wendy-kobayashi" target="_blank" rel="noreferrer">
+            <FontAwesomeIcon icon={faLinkedin} />
+          </a>
+          <a className="btn-floating btn-link"  href="https://twitter.com/iamalilforest" target="_blank" rel="noreferrer">
+            <FontAwesomeIcon icon={faTwitter} />
+          </a>
+        </div>
+      </div>
     </footer>
   );
 }
