b4065c82486d3f4c3eb7280f5d6e9f018d1d19de
Wendy Kobayashi <iamalittleforest@gmail.com>
2021-07-30 16:19:00 -0700

    define format of navigation

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -4,7 +4,22 @@ import '../styles/Navigation.css';
 export default function Navigation() {
   return (
     <div>
+      <nav className="navigation">
+        <div class="container nav-wrapper">  
+          <a href="#" data-target="mobile-demo" className="sidenav-trigger"><i class="fas fa-bars"></i></a>
+          <ul id="nav-mobile" className="right hide-on-med-and-down">
+            <li><a className="nav-link" href="#about-container" alt="About">ABOUT</a></li>
+            <li><a className="nav-link" href="#portfolio-container" alt="Portfolio">PORTFOLIO</a></li>
+            <li><a className="nav-link" href="#contact-container" alt="Contact">CONTACT</a></li>
+          </ul>
+        </div>
+      </nav>
 
+      <ul class="sidenav" id="mobile-demo">
+        <li><a className="nav-link" href="#about-container" alt="About">ABOUT</a></li>
+        <li><a className="nav-link" href="#portfolio-container" alt="Portfolio">PORTFOLIO</a></li>
+        <li><a className="nav-link" href="#contact-container" alt="Contact">CONTACT</a></li>
+      </ul>
     </div>
   );
 }
