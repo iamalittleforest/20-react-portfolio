ea55cc8ded13f63a98dd2bf1890e00d63f3ce5d9
Wendy Kobayashi <iamalittleforest@gmail.com>
2021-07-30 14:00:28 -0700

    add additional style classes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,16 +5,16 @@ import '../styles/Footer.css';
 
 export default function Footer() {
   return (
-    <footer className="page-footer footer">
+    <footer className="footer">
       <div className="container">
         <div className="row center row-padding">
           <a className="btn-floating btn-link" href="https://www.github.com/iamalittleforest" target="_blank" rel="noreferrer">
             <FontAwesomeIcon icon={faGithub} />
           </a>
-          <a className="btn-floating btn-link"  href="https://www.linkedin.com/in/wendy-kobayashi" target="_blank" rel="noreferrer">
+          <a className="btn-floating btn-link" href="https://www.linkedin.com/in/wendy-kobayashi" target="_blank" rel="noreferrer">
             <FontAwesomeIcon icon={faLinkedin} />
           </a>
-          <a className="btn-floating btn-link"  href="https://twitter.com/iamalilforest" target="_blank" rel="noreferrer">
+          <a className="btn-floating btn-link" href="https://twitter.com/iamalilforest" target="_blank" rel="noreferrer">
             <FontAwesomeIcon icon={faTwitter} />
           </a>
         </div>
