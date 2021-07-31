09645d75424427bcbc664af9ceb695147d75c86c
Wendy Kobayashi <iamalittleforest@gmail.com>
2021-07-30 17:25:13 -0700

    fixed functionality of mobile navigation by converting to to class component

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,29 +1,39 @@
 import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faBars } from '@fortawesome/free-solid-svg-icons'
+import M from 'materialize-css/dist/js/materialize.min.js';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
 import '../styles/Navigation.css';
 
-export default function Navigation() {
-  return (
-    <div>
-      <nav className="navigation">
-        <div class="container nav-wrapper">  
-          <a href="#" data-target="mobile-demo" className="sidenav-trigger">
-            <FontAwesomeIcon icon={faBars} />
-          </a>
-          <ul id="nav-mobile" className="right hide-on-med-and-down">
-            <li><a className="nav-link" href="#about-container" alt="About">ABOUT</a></li>
-            <li><a className="nav-link" href="#portfolio-container" alt="Portfolio">PORTFOLIO</a></li>
-            <li><a className="nav-link" href="#contact-container" alt="Contact">CONTACT</a></li>
-          </ul>
-        </div>
-      </nav>
+export default class Navigation extends React.Component {
+  componentDidMount() {
+    document.addEventListener('DOMContentLoaded', function () {
+      let elems = document.querySelectorAll('.sidenav');
+      M.Sidenav.init(elems, {});
+    });
+  }
 
-      <ul class="sidenav" id="mobile-demo">
-        <li><a className="nav-link" href="#about-container" alt="About">ABOUT</a></li>
-        <li><a className="nav-link" href="#portfolio-container" alt="Portfolio">PORTFOLIO</a></li>
-        <li><a className="nav-link" href="#contact-container" alt="Contact">CONTACT</a></li>
-      </ul>
-    </div>
-  );
+  render() {
+    return (
+      <div>
+        <nav className="navigation">
+          <div class="container nav-wrapper">
+            <a href="#" data-target="mobile-demo" className="sidenav-trigger">
+              <FontAwesomeIcon icon={faBars} />
+            </a>
+            <ul id="nav-mobile" className="right hide-on-med-and-down">
+              <li><a className="nav-link" href="#about-container" alt="About">ABOUT</a></li>
+              <li><a className="nav-link" href="#portfolio-container" alt="Portfolio">PORTFOLIO</a></li>
+              <li><a className="nav-link" href="#contact-container" alt="Contact">CONTACT</a></li>
+            </ul>
+          </div>
+        </nav>
+
+        <ul class="sidenav" id="mobile-demo">
+          <li><a className="nav-link" href="#about-container" alt="About">ABOUT</a></li>
+          <li><a className="nav-link" href="#portfolio-container" alt="Portfolio">PORTFOLIO</a></li>
+          <li><a className="nav-link" href="#contact-container" alt="Contact">CONTACT</a></li>
+        </ul>
+      </div>
+    );
+  }
 }
