850bc55c1f9cf1677f99385fa9523332ff8e5766
Wendy Kobayashi <iamalittleforest@gmail.com>
2021-07-30 23:23:04 -0700

    align classes to css file

--- src/components/Project.js
+++ src/components/Project.js
@@ -4,6 +4,7 @@ import React from 'react';
 // import css
 import '../styles/components.css';
 
+// display project image, title, description, tech, deployed link and repo link
 function Project(props) {
   return (
     <div className="card project-card">
@@ -16,9 +17,9 @@ function Project(props) {
           <p className="project-description">{props.description}</p>
           <p className="project-tech">{props.tech}</p>
         </div>
-        <div className="card-action project-links">
-          <a className="links" href={props.deployed} target="_blank" rel="noreferrer">Deployed App</a>
-          <a className="links" href={props.repo} target="_blank" rel="noreferrer">GitHub Repo</a>
+        <div className="card-action project-link-container">
+          <a className="project-link" href={props.deployed} target="_blank" rel="noreferrer">Deployed App</a>
+          <a className="project-link" href={props.repo} target="_blank" rel="noreferrer">GitHub Repo</a>
         </div>
       </div>
     </div>
