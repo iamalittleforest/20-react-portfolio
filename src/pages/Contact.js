dd5de34c48af46348c635262ee0b6251148c13b5
Wendy Kobayashi <iamalittleforest@gmail.com>
2021-07-30 22:27:19 -0700

    define format of contact

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -2,8 +2,39 @@ import React from 'react';
 
 export default function Contact() {
   return (
-    <div>
-
+    <div className="contact">
+      <div className="container">
+        <div className="row row-padding">
+          <h5 className="container-title">
+            Contact
+          </h5>
+          <form className="col s12 ">
+            <div className="row">
+              <div className="input-field col s6 offset-m3">
+                <input id="name" type="text" className="validate" required />
+                <label for="name">Name</label>
+              </div>
+            </div>
+            <div className="row">
+              <div className="input-field col s6 offset-m3">
+                <input id="email" type="email" className="validate" required />
+                <label for="email">Email</label>
+              </div>
+            </div>
+            <div className="row">
+              <div className="input-field col s6 offset-m3">
+                <textarea id="message" className="materialize-textarea validate" required></textarea>
+                <label for="message">Message</label>
+              </div>
+            </div>
+            <div className="row">
+              <div className="input-field col s12">
+                <button className="btn waves-effect waves-light" type="submit" name="action">Submit</button>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
     </div>
   );
 }
