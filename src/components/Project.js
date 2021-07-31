0ce24a530a9153f82fa266eed07b8681c13229b1
Wendy Kobayashi <iamalittleforest@gmail.com>
2021-07-30 23:06:50 -0700

    add comments and refactor function export

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,23 +1,28 @@
+// import react
 import React from 'react';
-import '../styles/Project.css';
 
-export default function Project(props) {
+// import css
+import '../styles/components.css';
+
+function Project(props) {
   return (
     <div className="card project-card">
-    <div className="card-image">
-      <img className="project-img" src={props.image} alt={props.alt}/>
-    </div>
-    <div className="card-stacked">
-      <div className="card-content project-container">
-        <span className="card-title project-title">{props.title}</span>
-        <p className="project-description">{props.description}</p>
-        <p className="project-tech">{props.tech}</p>
+      <div className="card-image">
+        <img className="project-img" src={props.image} alt={props.alt} />
       </div>
-      <div className="card-action project-links">
-        <a className="links" href={props.deployed} target="_blank" rel="noreferrer">Deployed App</a>
-        <a className="links" href={props.repo} target="_blank" rel="noreferrer">GitHub Repo</a>
+      <div className="card-stacked">
+        <div className="card-content project-container">
+          <span className="card-title project-title">{props.title}</span>
+          <p className="project-description">{props.description}</p>
+          <p className="project-tech">{props.tech}</p>
+        </div>
+        <div className="card-action project-links">
+          <a className="links" href={props.deployed} target="_blank" rel="noreferrer">Deployed App</a>
+          <a className="links" href={props.repo} target="_blank" rel="noreferrer">GitHub Repo</a>
+        </div>
       </div>
     </div>
-  </div>
   );
 }
+
+export default Project;
