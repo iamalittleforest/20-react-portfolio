ca53c533cc730e942f754fcb5433d530f73c075d
Wendy Kobayashi <iamalittleforest@gmail.com>
2021-07-31 22:33:18 -0700

    fix comment and formatting

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,4 +1,4 @@
-// import react
+// import React
 import React from 'react';
 
 // import css
@@ -7,19 +7,19 @@ import '../styles/components.css';
 // display project image, title, description, tech, deployed link and repo link
 function Project(props) {
   return (
-    <div className="card project-card">
-      <div className="card-image">
-        <img className="project-img" src={props.image} alt={props.alt} />
+    <div className='card project-card'>
+      <div className='card-image'>
+        <img className='project-img' src={props.image} alt={props.alt} />
       </div>
-      <div className="card-stacked">
-        <div className="card-content project-container">
-          <span className="card-title project-title">{props.title}</span>
-          <p className="project-description">{props.description}</p>
-          <p className="project-tech">{props.tech}</p>
+      <div className='card-stacked'>
+        <div className='card-content project-container'>
+          <span className='card-title project-title'>{props.title}</span>
+          <p className='project-description'>{props.description}</p>
+          <p className='project-tech'>{props.tech}</p>
         </div>
-        <div className="card-action project-link-container">
-          <a className="project-link" href={props.deployed} target="_blank" rel="noreferrer">Deployed App</a>
-          <a className="project-link" href={props.repo} target="_blank" rel="noreferrer">GitHub Repo</a>
+        <div className='card-action project-link-container'>
+          <a className='project-link' href={props.deployed} target='_blank' rel='noreferrer'>Deployed App</a>
+          <a className='project-link' href={props.repo} target='_blank' rel='noreferrer'>GitHub Repo</a>
         </div>
       </div>
     </div>
