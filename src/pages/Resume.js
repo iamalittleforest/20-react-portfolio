56b7232b02e34b6d2b71c8dd2a80ad2c03fe61e2
Wendy Kobayashi <iamalittleforest@gmail.com>
2021-08-01 12:17:18 -0700

    adjust font size and content

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -14,12 +14,12 @@ function Resume() {
     <div className='resume'>
       <div className='container'>
         <div className='row center row-padding'>
-          <h5 className='container-title'>Resume</h5>
-          <h3 className='resume-heading'>Proficiencies:</h3>
+          <h4 className='container-title'>Resume</h4>
+          <h5 className='resume-heading'>Proficiencies:</h5>
           <div className='proficiencies'>
             HTML, CSS, JavaScript, jQuery, Bootstrap/Materialize, Handlebars, Responsive Design, RESTful APIs, Git/GitHub, Heroku, Node.js, Express, User Authentication, MySQL, MongoDB
           </div>
-          <h3 className='resume-heading'>Download Resume:</h3>
+          <h5 className='resume-heading'>Download Link:</h5>
           <a className='btn-floating grey darken-3 resume-link' href='../assets/files/WendyKobayashi_Resume.pdf' download>
             <FontAwesomeIcon icon={faFilePdf} size='lg' />
           </a>
