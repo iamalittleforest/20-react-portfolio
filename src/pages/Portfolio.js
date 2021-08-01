55ad45be160a266a094ad1b9133ee6046dccf2b8
Wendy Kobayashi <iamalittleforest@gmail.com>
2021-07-31 21:44:14 -0700

    add project map

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -2,6 +2,10 @@ import React from 'react';
 import Project from './Project';
 import '../styles/pages.css';
 
+const projects = [
+
+]
+
 function Portfolio() {
   return (
     <div className='portfolio'>
@@ -10,7 +14,19 @@ function Portfolio() {
           <h5 className='container-title'>
             Portfolio
           </h5>
-          <Project />
+          <div>
+            {projects.map((project) => (
+              <Project
+                image={project.image}
+                alt={project.alt}
+                title={project.title}
+                description={project.description}
+                tech={project.tech}
+                deployed={project.deployed}
+                repo={project.repo}
+              />
+            ))}
+          </div>
         </div>
       </div>
     </div>
