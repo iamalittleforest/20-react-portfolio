b9785d60b499e67b2323580c167ff67df6e139aa
Wendy Kobayashi <iamalittleforest@gmail.com>
2021-07-31 17:36:16 -0700

    fix link icon formatting and styling

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -11,17 +11,17 @@ import '../styles/components.css';
 // links to GitHub, LinkedIn, and Twitter profiles
 function Footer() {
   return (
-    <footer className="footer">
-      <div className="container">
-        <div className="row center row-padding">
-          <a className="btn-floating footer-link" href="https://www.github.com/iamalittleforest" target="_blank" rel="noreferrer">
-            <FontAwesomeIcon icon={faGithub} />
+    <footer className='footer'>
+      <div className='container'>
+        <div className='row center row-padding'>
+          <a className='btn-floating grey darken-3 footer-link' href='https://www.github.com/iamalittleforest' target='_blank' rel='noreferrer'>
+            <FontAwesomeIcon icon={faGithub} size='lg' />
           </a>
-          <a className="btn-floating footer-link" href="https://www.linkedin.com/in/wendy-kobayashi" target="_blank" rel="noreferrer">
-            <FontAwesomeIcon icon={faLinkedin} />
+          <a className='btn-floating grey darken-3 footer-link' href='https://www.linkedin.com/in/wendy-kobayashi' target='_blank' rel='noreferrer'>
+            <FontAwesomeIcon icon={faLinkedin} size='lg' />
           </a>
-          <a className="btn-floating footer-link" href="https://twitter.com/iamalilforest" target="_blank" rel="noreferrer">
-            <FontAwesomeIcon icon={faTwitter} />
+          <a className='btn-floating grey darken-3 footer-link' href='https://twitter.com/iamalilforest' target='_blank' rel='noreferrer'>
+            <FontAwesomeIcon icon={faTwitter} size='lg' />
           </a>
         </div>
       </div>
