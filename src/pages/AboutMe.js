7970084da7b3eb27a40f48a794944d10963968fc
Wendy Kobayashi <iamalittleforest@gmail.com>
2021-07-31 18:01:55 -0700

    define format of AboutMe

--- src/pages/AboutMe.js
+++ src/pages/AboutMe.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import '../styles/pages.css';
 
-export default function AboutMe() {
+function AboutMe() {
   return (
-    <div>
-
+    <div className='about'>
+      <div className='container'>
+        <div className='row center row-padding'>
+          <h5 className='container-title'>
+            About Me
+          </h5>
+          <img className='about-img circle' src='../images/profile.jpg' alt='Profile' />
+          <p className='about-description'>
+            Hello! My name is Wendy Kobayashi. I am a mechanical engineer turned full stack web developer with 4+ years as a regulatory professional and 6+ years as an engineer in the medical device industry. 
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
+
+export default AboutMe;
