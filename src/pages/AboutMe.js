173e2b1741507460ed4abe2e5d4b0612d12b64e7
Wendy Kobayashi <iamalittleforest@gmail.com>
2021-07-31 18:25:35 -0700

    fix capitalization of profile photo

--- src/pages/AboutMe.js
+++ src/pages/AboutMe.js
@@ -9,7 +9,7 @@ function AboutMe() {
           <h5 className='container-title'>
             About Me
           </h5>
-          <img className='about-img circle' src='../images/profile.jpg' alt='Profile' />
+          <img className='about-img circle' src='../images/Profile.jpg' alt='Profile' />
           <p className='about-description'>
             Hello! My name is Wendy Kobayashi. I am a mechanical engineer turned full stack web developer with 4+ years as a regulatory professional and 6+ years as an engineer in the medical device industry. 
           </p>
