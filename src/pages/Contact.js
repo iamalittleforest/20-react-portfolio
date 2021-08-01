91ad62e0ace58ca2df50f068ce91f13ba05e84f5
Wendy Kobayashi <iamalittleforest@gmail.com>
2021-07-31 21:07:00 -0700

    add comments and custom css classes

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,6 +1,10 @@
+// import React
 import React from 'react';
+
+// import css
 import '../styles/pages.css';
 
+// form with fields for name, email, and message (w/validation)
 function Contact() {
   return (
     <div className='contact'>
@@ -10,28 +14,28 @@ function Contact() {
             Contact
           </h5>
           <form className='col s12'>
-            <div className='row'>
+            <div className='row field-row'>
               <div className='input-field col s6 offset-m3'>
                 <input id='name' type='text' className='validate' required />
                 <label for='name'>Name</label>
                 <span className='helper-text' data-error='Please provide your name' data-success=''></span>
               </div>
             </div>
-            <div className='row'>
+            <div className='row field-row'>
               <div className='input-field col s6 offset-m3'>
                 <input id='email' type='email' className='validate' required />
                 <label for='email'>Email</label>
                 <span className='helper-text' data-error='Please provide a valid email address' data-success=''></span>
               </div>
             </div>
-            <div className='row'>
+            <div className='row field-row'>
               <div className='input-field col s6 offset-m3'>
                 <textarea id='message' className='materialize-textarea validate' required></textarea>
                 <label for='message'>Message</label>
                 <span className='helper-text' data-error='Please provide a message' data-success=''></span>
               </div>
             </div>
-            <div className='row'>
+            <div className='row field-row'>
               <div className='input-field col s12'>
                 <button className='btn waves-effect waves-light' type='submit' name='action'>Submit</button>
               </div>
