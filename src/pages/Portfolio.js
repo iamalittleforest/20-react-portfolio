7189bdb94de5c25d1b65a5bc37c44d47d12b79bf
Wendy Kobayashi <iamalittleforest@gmail.com>
2021-08-01 21:36:24 -0700

    add import of project images

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -4,7 +4,13 @@ import React from 'react';
 // import Project component
 import Project from '../components/Project';
 
-// import css
+// import images and css
+import progressiveBudgetImg from '../assets/images/Progressive-Budget.png';
+import fitnessTrackerImg from '../assets/images/Fitness-Tracker.png';
+import petConnectImg from '../assets/images/Pet-Connect.png';
+import techBlogImg from '../assets/images/Tech-Blog.png';
+import makeItBuyItImg from '../assets/images/MakeIt-BuyIt.png';
+import weatherDashboardImg from '../assets/images/Weather-Dashboard.png';
 import '../styles/pages.css';
 
 // list projects (image, title, description, tech, and links to deployed app and repo)
@@ -13,7 +19,7 @@ function Portfolio() {
   // array of projects
   const projects = [
     {
-      image: "../assets/images/Progressive-Budget.png",
+      image: progressiveBudgetImg,
       alt: "Screenshot of Progressive Budget App",
       title: "Progressive Budget",
       description: "Progressive Budget is an application that enables a user to add expenses and deposits to their budget. The application is able to be accessed offline and will populate the total when the connection is reestablished.",
@@ -23,7 +29,7 @@ function Portfolio() {
       id: 1
     },
     {
-      image: "../assets/images/Fitness-Tracker.png",
+      image: fitnessTrackerImg,
       alt: "Screenshot of Fitness Tracker App",
       title: "Fitness Tracker",
       description: "Fitness Tracker is an application that enables a user to view, create, and track daily workouts. The application also has a dashboard feature that displays historical views of the total duration and total weight of each workout.",
@@ -33,7 +39,7 @@ function Portfolio() {
       id: 2
     },
     {
-      image: "../assets/images/Pet-Connect.png",
+      image: petConnectImg,
       alt: "Screenshot of Pet Connect App",
       title: "Pet Connect",
       description: "Pet Connect is a social media application that is designed to connect pet owners. Users can create posts to plan dog walks with others, get potty training lessons, provide product information, and ask questions from other pet owners.",
@@ -43,7 +49,7 @@ function Portfolio() {
       id: 3
     },
     {
-      image: "../assets/images/Tech-Blog.png",
+      image: techBlogImg,
       alt: "Screenshot of Tech Blog App",
       title: "Tech Blog",
       description: "Tech Blog is a CMS-style blog site that can be used to share technical concepts, recent advancements, and new technologies. Users can publish blog posts and comment on other users' posts.",
@@ -53,7 +59,7 @@ function Portfolio() {
       id: 4
     },
     {
-      image: "../assets/images/MakeIt-BuyIt.png",
+      image: makeItBuyItImg,
       alt: "Screenshot of Make It / Buy It App",
       title: "Make It / Buy It",
       description: "Make it / Buy It is an application that is designed to help users who need help with deciding on their next meal. If a user decides to “Make It,” they will navigate to a page that will pull up recipes based on ingredients that the user wants to use. On the other hand, if the user decides to “Buy It,” they will navigate to a page that will pull up restaurants based on a location the user chooses.",
@@ -63,7 +69,7 @@ function Portfolio() {
       id: 5
     },
     {
-      image: "../assets/images/Weather-Dashboard.png",
+      image: weatherDashboardImg,
       alt: "Screenshot of Weather Dashboard App",
       title: "Weather Dashboard",
       description: "Weather Dashboard is an application that enables users to view the current weather and future forecast for a city they search for. Users can also select cities from their search history to view the current weather and future forecast for that city.",
@@ -77,7 +83,7 @@ function Portfolio() {
   return (
     <div className='portfolio'>
       <div className='container'>
-        <div className='row center row-padding'>
+        <div className='center row-padding'>
           <h4 className='container-title'>Portfolio</h4>
           <div>
             {projects.map((project) => (
