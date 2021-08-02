6285ca11b85844b15f1a106d3a9aec71bad454b3
Wendy Kobayashi <iamalittleforest@gmail.com>
2021-08-01 22:03:52 -0700

    fix formatting of row

--- src/pages/AboutMe.js
+++ src/pages/AboutMe.js
@@ -15,7 +15,7 @@ function AboutMe() {
           <div className="row">
             <img className='about-img circle' src={profileImg} alt='Profile' />
           </div>
-          <div className='row about-description'>
+          <div className='about-description'>
             Hello! My name is Wendy Kobayashi. I am a mechanical engineer turned full stack web developer with 4+ years as a regulatory professional and 6+ years as an engineer in the medical device industry.
           </div>
         </div>
