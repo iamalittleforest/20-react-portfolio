02dfa801e7e9d12b2244b1e3cc6cdacf6ae1e543
Wendy Kobayashi <iamalittleforest@gmail.com>
2021-08-02 22:58:10 -0700

    fix resume download

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -1,10 +1,6 @@
 // import React
 import React from 'react';
 
-// import icons from Font Awesome 
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faFilePdf } from '@fortawesome/free-solid-svg-icons';
-
 // import css
 import '../styles/pages.css';
 
@@ -13,15 +9,14 @@ function Resume() {
   return (
     <div className='resume'>
       <div className='container'>
-        <div className='row center row-padding'>
+        <div className='center row-padding'>
           <h4 className='container-title'>Resume</h4>
           <h5 className='resume-heading'>Proficiencies:</h5>
           <div className='proficiencies'>
             HTML, CSS, JavaScript, jQuery, Bootstrap/Materialize, Handlebars, Responsive Design, RESTful APIs, Git/GitHub, Heroku, Node.js, Express, User Authentication, MySQL, MongoDB
           </div>
-          <h5 className='resume-heading'>Download Link:</h5>
-          <a className='btn-floating grey darken-3 resume-link' href='../assets/files/WendyKobayashi_Resume.pdf' download>
-            <FontAwesomeIcon icon={faFilePdf} size='lg' />
+          <a className='btn waves-effect waves-light grey darken-1 resume-link' href='https://github.com/iamalittleforest/20-react-portfolio/raw/main/src/assets/files/Wendy%20Kobayashi_Resume.pdf' download>
+            Download Resume
           </a>
         </div>
       </div>
