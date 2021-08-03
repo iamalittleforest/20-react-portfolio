408ecba847bdb45f645881181ee1f30e00c13eed
Wendy Kobayashi <iamalittleforest@gmail.com>
2021-08-02 23:06:13 -0700

    convert from class component to functional component, enable functionality of navigation links

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,7 +1,7 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 
 // import react
-import React, { Component } from 'react';
+import React, { useEffect } from 'react';
 
 // import Materialize
 import M from 'materialize-css/dist/js/materialize.min.js';
@@ -14,42 +14,70 @@ import { faBars } from '@fortawesome/free-solid-svg-icons';
 import '../styles/components.css';
 
 // links to About, Portfolio, Contact, and Resume pages
-class Navigation extends Component {
+function Navigation(props) {
 
   // sidenav functionality
-  componentDidMount() {
-    document.addEventListener('DOMContentLoaded', function () {
-      let elems = document.querySelectorAll('.sidenav');
-      M.Sidenav.init(elems, {});
-    });
-  }
+  useEffect(() => {
+    let elems = document.querySelectorAll('.sidenav');
+    M.Sidenav.init(elems, {});
+  }, []);
 
-  render() {
-    return (
-      <div>
-        <nav className="navigation">
-          <div className="container nav-wrapper">
-            <a href="#" data-target="mobile-demo" className="sidenav-trigger">
-              <FontAwesomeIcon icon={faBars} />
-            </a>
-            <ul id="nav-mobile" className="right hide-on-med-and-down">
-              <li><a className="nav-link" href="#about-container" alt="About">About</a></li>
-              <li><a className="nav-link" href="#portfolio-container" alt="Portfolio">Portfolio</a></li>
-              <li><a className="nav-link" href="#contact-container" alt="Contact">Contact</a></li>
-              <li><a className="nav-link" href="#resume-container" alt="Resume">Resume</a></li>
-            </ul>
-          </div>
-        </nav>
+  return (
+    <div>
+      <nav className='navigation'>
+        <div className='container nav-wrapper'>
+          <a href='#' data-target='mobile-demo' className='sidenav-trigger'>
+            <FontAwesomeIcon icon={faBars} />
+          </a>
+          <ul id='nav-mobile' className='right hide-on-med-and-down'>
+            <li>
+              <a className={props.currentPage === 'AboutMe' ? 'nav-link current' : 'nav-link'} href='#about' onClick={() => props.handlePageChange('AboutMe')}>
+                About Me
+              </a>
+            </li>
+            <li>
+              <a className={props.currentPage === 'Portfolio' ? 'nav-link current' : 'nav-link'} href='#portfolio' onClick={() => props.handlePageChange('Portfolio')}>
+                Portfolio
+              </a>
+            </li>
+            <li>
+              <a className={props.currentPage === 'Contact' ? 'nav-link current' : 'nav-link'} href='#contact' onClick={() => props.handlePageChange('Contact')}>
+                Contact
+              </a>
+            </li>
+            <li>
+              <a className={props.currentPage === 'Resume' ? 'nav-link current' : 'nav-link'} href='#resume' onClick={() => props.handlePageChange('Resume')}>
+                Resume
+              </a>
+            </li>
+          </ul>
+        </div>
+      </nav>
 
-        <ul className="sidenav" id="mobile-demo">
-          <li><a className="nav-link" href="#about-container" alt="About">About</a></li>
-          <li><a className="nav-link" href="#portfolio-container" alt="Portfolio">Portfolio</a></li>
-          <li><a className="nav-link" href="#contact-container" alt="Contact">Contact</a></li>
-          <li><a className="nav-link" href="#contact-resume" alt="Resume">Resume</a></li>
-        </ul>
-      </div>
-    );
-  }
+      <ul className='sidenav' id='mobile-demo'>
+        <li>
+          <a className={props.currentPage === 'AboutMe' ? 'nav-link current' : 'nav-link'} href='#about' onClick={() => props.handlePageChange('AboutMe')}>
+            About Me
+          </a>
+        </li>
+        <li>
+          <a className={props.currentPage === 'Portfolio' ? 'nav-link current' : 'nav-link'} href='#portfolio' onClick={() => props.handlePageChange('Portfolio')}>
+            Portfolio
+          </a>
+        </li>
+        <li>
+          <a className={props.currentPage === 'Contact' ? 'nav-link current' : 'nav-link'} href='#contact' onClick={() => props.handlePageChange('Contact')}>
+            Contact
+          </a>
+        </li>
+        <li>
+          <a className={props.currentPage === 'Resume' ? 'nav-link current' : 'nav-link'} href='#resume' onClick={() => props.handlePageChange('Resume')}>
+            Resume
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
 }
 
 export default Navigation;
